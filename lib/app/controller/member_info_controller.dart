d9e743fa3c2c612548431f803c275b45514ca8ac
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-02-27 21:32:33 +0900

    albert reinforced

--- lib/app/controller/member_info_controller.dart
+++ lib/app/controller/member_info_controller.dart
@@ -19,28 +19,35 @@ class MemberInfoController {
     List lessonActionNoteList =
         await lessonRepository.getLessonActionNote(uid, memberId);
 
+    print('###Albert, tell me ${lessonDayNoteList.length}');
+
+    print('###Albert, tell me ${lessonActionNoteList.length}');
+
     lessonDayNoteList.forEach((lNelement) {
       lessonActionNoteList.forEach((aNelement) {
-        lessonNoteInfo = LessonNoteInfo(
-          uid,
-          memberId,
-          lNelement['name'],
-          lNelement['phoneNumber'],
-          lNelement['lessonDate'],
-          aNelement['actionName'],
-          aNelement['grade'],
-          aNelement['pos'],
-          aNelement['totalNote'],
-          aNelement['apratusName'],
-          aNelement['timestamp'],
-          lNelement['todayNote'],
-          lNelement['timestamp'],
-          aNelement['totalNote'].toString().isNotEmpty ? true : false,
-          aNelement['id'],
-          lNelement['id'],
-        );
+        if (lNelement['lessonDate'] == aNelement['lessonDate']) {
+          lessonNoteInfo = LessonNoteInfo(
+            uid,
+            memberId,
+            lNelement['name'],
+            lNelement['phoneNumber'],
+            lNelement['lessonDate'],
+            aNelement['actionName'],
+            aNelement['grade'],
+            aNelement['pos'],
+            aNelement['totalNote'],
+            aNelement['apratusName'],
+            aNelement['timestamp'],
+            lNelement['todayNote'],
+            lNelement['timestamp'],
+            aNelement['totalNote'].toString().isNotEmpty ? true : false,
+            aNelement['id'],
+            lNelement['id'],
+          );
 
-        lessonNoteInfoList.add(lessonNoteInfo);
+          lessonNoteInfoList.add(lessonNoteInfo);
+        }
+
         // print(
         //     "C'MON ALBERT - uid : ${uid}, memberId : ${memberId}, lNelement['name'] : ${lNelement['name']}, lNelement['phoneNumber'] : ${lNelement['phoneNumber']}, lNelement['lessonDate'] : ${lNelement['lessonDate']}, aNelement['actionName'] : ${aNelement['actionName']}, aNelement['grade'] : ${aNelement['grade']}, aNelement['pos'] : ${aNelement['pos']}, aNelement['totalNote'] : ${aNelement['totalNote']}, aNelement['apratusName'] : ${aNelement['apratusName']}, aNelement['timestamp'] : ${aNelement['timestamp']}, lNelement['todayNote'] : ${lNelement['todayNote']}, lNelement['timestamp'] : ${lNelement['timestamp']}, false, aNelement['id'] : ${aNelement['id']}, lNelement['id'] : ${lNelement['id']}");
       });
