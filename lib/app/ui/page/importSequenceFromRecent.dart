f1297adefbbad4c838a2e2bd323ad626bfb63a88
정규호 <emotionalmachine@jeong-gyuhoui-MacBookPro.local>
2023-02-28 17:08:33 +0900

    code merge 2023-02-28 17:08

--- lib/app/ui/page/importSequenceFromRecent.dart
+++ lib/app/ui/page/importSequenceFromRecent.dart
@@ -4,6 +4,9 @@ import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:provider/provider.dart';
+import 'package:web_project/app/data/provider/auth_service.dart';
+import 'package:web_project/app/data/provider/sequenceRecent_service.dart';
 import 'package:web_project/app/ui/widget/actionListTileWidget.dart';
 import 'package:web_project/app/ui/widget/centerConstraintBody.dart';
 import 'package:web_project/app/data/model/color.dart';
@@ -119,140 +122,150 @@ class _ImportSequenceFromRecentState extends State<ImportSequenceFromRecent> {
   int selectedCnt = 0;
   /**선택된 동작 리스트 -> 추후 돌려줄 것임 */
   List selectedActionList = [];
+  /**sequenceRecentService.read() 의 결과 값 리스트 */
+  List recentSequenceRusltList = [];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: BaseAppBarMethod(context, "시퀀스 불러오기", null, null, null),
-      body: CenterConstrainedBody(
-        child: Stack(
-          alignment: Alignment.bottomCenter,
-          children: [
-            Column(
+      body: Consumer<SequenceRecentService>(
+        builder: (context, sequenceRecentService, child) {
+          sequenceRecentService.read(AuthService().currentUser()!.uid).then((value){
+            
+          });
+          return CenterConstrainedBody(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
               children: [
-                /// 헤더
-                Container(
-                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
-                  child: Row(children: [
-                    Text(
-                      '시퀀스 제목',
-                      style: TextStyle(
-                          fontSize: 20,
-                          color: Palette.gray00,
-                          fontWeight: FontWeight.bold),
+                Column(
+                  children: [
+                    /// 헤더
+                    Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                      child: Row(children: [
+                        Text(
+                          '시퀀스 제목',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: Palette.gray00,
+                              fontWeight: FontWeight.bold),
+                        ),
+                        Spacer(),
+                        Text('편집',
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: Palette.textBlue,
+                            ))
+                      ]),
                     ),
-                    Spacer(),
-                    Text('편집',
-                        style: TextStyle(
-                          fontSize: 16,
-                          color: Palette.textBlue,
-                        ))
-                  ]),
-                ),
 
-                /// 리스트들이 뿌려지는 영역
-                Expanded(
-                  child: ReorderableListView.builder(
-                    padding: EdgeInsets.only(bottom: 100),
-                    onReorder: (oldIndex, newIndex) {
-                      if (newIndex > oldIndex) {
-                        newIndex -= 1;
-                      }
-                      final movedActionList = actionList.removeAt(oldIndex);
-                      actionList.insert(newIndex, movedActionList);
-
-                      setState(() {});
-                    },
-                    physics: BouncingScrollPhysics(),
-                    itemCount: actionList.length,
-                    itemBuilder: (context, index) {
-                      Key? valueKey;
-                      actionList[index]['index'] = index;
-                      valueKey = ValueKey(actionList[index]['index']);
-
-                      return ActionListTile(
-                        key: valueKey,
-                        isSelectable: true,
-                        isSelected: actionList[index]['selected'],
-                        actionList: actionList,
-                        isDraggable: true,
-                        actionName: actionList[index]['actionName'],
-                        apparatus: actionList[index]['apparatus'],
-                        position: actionList[index]['position'],
-                        name: "",
-                        phoneNumber: "",
-                        lessonDate: "",
-                        grade: "",
-                        totalNote: "",
-                        docId: "",
-                        memberdocId: "",
-                        uid: "",
-                        pos: index,
-                        customFunctionOnTap: () {
-                          actionList[index]['selected'] =
-                              !actionList[index]['selected'];
-
-                          if (actionList[index]['selected']) {
-                            selectedCnt++;
-                          } else {
-                            selectedCnt--;
+                    /// 리스트들이 뿌려지는 영역
+                    Expanded(
+                      child: ReorderableListView.builder(
+                        padding: EdgeInsets.only(bottom: 100),
+                        onReorder: (oldIndex, newIndex) {
+                          if (newIndex > oldIndex) {
+                            newIndex -= 1;
                           }
-
-                          print('####여기부터 봐라####');
-                          print('seletedCnt: $selectedCnt');
-                          print('index: $index');
-                          print('actionList[index]: ${actionList[index]}');
-                          print('actionList: $actionList');
-                          print('selectedActionList: $selectedActionList');
-                          print('####여기까지 봐라####');
+                          final movedActionList = actionList.removeAt(oldIndex);
+                          actionList.insert(newIndex, movedActionList);
 
                           setState(() {});
                         },
-                      );
-                    },
-                  ),
-                )
-              ],
-            ),
+                        physics: BouncingScrollPhysics(),
+                        itemCount: actionList.length,
+                        itemBuilder: (context, index) {
+                          Key? valueKey;
+                          actionList[index]['index'] = index;
+                          valueKey = ValueKey(actionList[index]['index']);
 
-            /// 불러오기 버튼을 아래쪽에 Stack
-            Offstage(
-              // offstage: selectedCnt == 0,
-              offstage: false,
-              child: Container(
-                alignment: Alignment.center,
-                height: 100,
-                // color: Palette.gray00.withOpacity(0.3), -> ActionSelector에서 쓰려고 만든거임
+                          return ActionListTile(
+                            key: valueKey,
+                            isSelectable: true,
+                            isSelected: actionList[index]['selected'],
+                            actionList: actionList,
+                            isDraggable: true,
+                            actionName: actionList[index]['actionName'],
+                            apparatus: actionList[index]['apparatus'],
+                            position: actionList[index]['position'],
+                            name: "",
+                            phoneNumber: "",
+                            lessonDate: "",
+                            grade: "",
+                            totalNote: "",
+                            docId: "",
+                            memberdocId: "",
+                            uid: "",
+                            pos: index,
+                            customFunctionOnTap: () {
+                              actionList[index]['selected'] =
+                                  !actionList[index]['selected'];
 
-                /// 불러오기 버튼
-                child: ElevatedButton(
-                  onPressed: () {
-                    selectedActionList
-                        .addAll(actionList.where((item) => item['selected']));
-                    // selectedActionList.add(actionList.where((item) => item['selected']));
-                    print(actionList.where((item) => item['selected']));
-                    print('selectedActionList: $selectedActionList');
-                    print('@@@@@ 전송! @@@@@');
+                              if (actionList[index]['selected']) {
+                                selectedCnt++;
+                              } else {
+                                selectedCnt--;
+                              }
 
-                    Navigator.pop(context);
-                  },
-                  style: ElevatedButton.styleFrom(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(30.0),
+                              print('####여기부터 봐라####');
+                              print('seletedCnt: $selectedCnt');
+                              print('index: $index');
+                              print('actionList[index]: ${actionList[index]}');
+                              print('actionList: $actionList');
+                              print('selectedActionList: $selectedActionList');
+                              print('####여기까지 봐라####');
+
+                              setState(() {});
+                            },
+                          );
+                        },
+                      ),
+                    )
+                  ],
+                ),
+
+                /// 불러오기 버튼을 아래쪽에 Stack
+                Offstage(
+                  // offstage: selectedCnt == 0,
+                  offstage: false,
+                  child: Container(
+                    alignment: Alignment.center,
+                    height: 100,
+                    // color: Palette.gray00.withOpacity(0.3), -> ActionSelector에서 쓰려고 만든거임
+
+                    /// 불러오기 버튼
+                    child: ElevatedButton(
+                      onPressed: () {
+                        selectedActionList.addAll(
+                            actionList.where((item) => item['selected']));
+                        // selectedActionList.add(actionList.where((item) => item['selected']));
+                        print(actionList.where((item) => item['selected']));
+                        print('selectedActionList: $selectedActionList');
+                        print('@@@@@ 전송! @@@@@');
+
+                        Navigator.pop(context);
+                      },
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30.0),
+                        ),
+                        elevation: 5,
+                        backgroundColor: Palette.buttonOrange,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 14, horizontal: 90),
+                        child: Text("불러오기", style: TextStyle(fontSize: 16)),
+                      ),
                     ),
-                    elevation: 5,
-                    backgroundColor: Palette.buttonOrange,
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 14, horizontal: 90),
-                    child: Text("불러오기", style: TextStyle(fontSize: 16)),
                   ),
                 ),
-              ),
+              ],
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
--- lib/app/ui/page/sequenceLibrary.dart
+++ lib/app/ui/page/sequenceLibrary.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:provider/provider.dart';
+import 'package:web_project/app/data/provider/auth_service.dart';
+import 'package:web_project/app/data/provider/sequenceCustom_service.dart';
+import 'package:web_project/app/data/provider/sequenceRecent_service.dart';
 import 'package:web_project/app/ui/page/importSequenceFromRecent.dart';
 import 'package:web_project/app/ui/page/importSequenceFromSaved.dart';
 import 'package:web_project/app/ui/widget/centerConstraintBody.dart';
 import 'package:web_project/app/data/model/color.dart';
 import 'package:web_project/app/ui/widget/globalWidget.dart';
+
+List recentSequenceList =[];
 
 class SequenceLibrary extends StatefulWidget {
   const SequenceLibrary({super.key});
@@ -14,6 +20,18 @@ class SequenceLibrary extends StatefulWidget {
 }
 
 class _SequenceLibraryState extends State<SequenceLibrary> {
+
+  @override
+  void initState() {
+    super.initState();
+    recentSequenceList =[];
+  }
+
+  @override
+  void dispose() {
+    recentSequenceList =[];
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -51,56 +69,11 @@ class _SequenceLibraryState extends State<SequenceLibrary> {
 
         /// 바디 시작
         body: CenterConstrainedBody(
-          child: TabBarView(children: [
-            /// 저장된 시퀀스 탭 내용
-            Container(
-              width: double.infinity,
-              child: ListView.builder(
-                physics: BouncingScrollPhysics(),
-                itemCount: 100,
-                itemBuilder: (context, index) {
-                  return ListTile(
-                    onTap: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => ImportSequenceFromSaved()),
-                      ).then((value) => Navigator.pop(context));
-                    },
-                    contentPadding:
-                        EdgeInsets.symmetric(vertical: 6, horizontal: 20),
-                    tileColor: Palette.mainBackground,
-                    title: Row(
-                      children: [
-                        Text('커스텀시퀀스 ${index + 1}'),
-                        SizedBox(width: 10),
-                        Container(
-                          padding:
-                              EdgeInsets.symmetric(vertical: 2, horizontal: 6),
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(10),
-                              border:
-                                  Border.all(color: Palette.gray99, width: 1)),
-                          child: Text(
-                            '${2 * index}',
-                            style: TextStyle(fontSize: 12),
-                          ),
-                        )
-                      ],
-                    ),
-                    trailing: Icon(
-                      Icons.arrow_forward_ios,
-                      size: 16,
-                    ),
-                  );
-                },
-              ),
-            ),
-
-            /// 최근 시퀀스 탭 내용
-            Container(
-              width: double.infinity,
-              child: Expanded(
+          child: TabBarView(
+            children: [
+              /// 저장된 시퀀스 탭 내용
+              Container(
+                width: double.infinity,
                 child: ListView.builder(
                   physics: BouncingScrollPhysics(),
                   itemCount: 100,
@@ -110,7 +83,7 @@ class _SequenceLibraryState extends State<SequenceLibrary> {
                         Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => ImportSequenceFromRecent()),
+                              builder: (context) => ImportSequenceFromSaved()),
                         ).then((value) => Navigator.pop(context));
                       },
                       contentPadding:
@@ -118,7 +91,7 @@ class _SequenceLibraryState extends State<SequenceLibrary> {
                       tileColor: Palette.mainBackground,
                       title: Row(
                         children: [
-                          Text('2022.03.22 22:31'),
+                          Text('커스텀시퀀스 ${index + 1}'),
                           SizedBox(width: 10),
                           Container(
                             padding: EdgeInsets.symmetric(
@@ -128,7 +101,7 @@ class _SequenceLibraryState extends State<SequenceLibrary> {
                                 border: Border.all(
                                     color: Palette.gray99, width: 1)),
                             child: Text(
-                              '40',
+                              '${2 * index}',
                               style: TextStyle(fontSize: 12),
                             ),
                           )
@@ -142,8 +115,68 @@ class _SequenceLibraryState extends State<SequenceLibrary> {
                   },
                 ),
               ),
-            ),
-          ]),
+
+              /// 최근 시퀀스 탭 내용
+              Consumer<SequenceRecentService>(
+                  builder: (context, sequenceRecentService, child) {
+                    recentSequenceList = [];
+                sequenceRecentService
+                    .read(AuthService().currentUser()!.uid)
+                    .then((value) {
+                      print("value : ${value.length}");
+                      recentSequenceList.addAll(value);
+                      print("recentSequenceList.length : ${recentSequenceList[1]['sequenceTitle']}");
+                      String sequenceTitle = recentSequenceList[1]['sequenceTitle'];
+                      print("sequenceTitle : ${sequenceTitle}");
+                      
+                    });
+                return Container(
+                  width: double.infinity,
+                  child: ListView.builder(
+                    physics: BouncingScrollPhysics(),
+                    itemCount: recentSequenceList.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) =>
+                                    ImportSequenceFromRecent()),
+                          ).then((value) => Navigator.pop(context));
+                        },
+                        contentPadding:
+                            EdgeInsets.symmetric(vertical: 6, horizontal: 20),
+                        tileColor: Palette.mainBackground,
+                        title: Row(
+                          children: [
+                            Text("머지?"),
+                            SizedBox(width: 10),
+                            Container(
+                              padding: EdgeInsets.symmetric(
+                                  vertical: 2, horizontal: 6),
+                              decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  border: Border.all(
+                                      color: Palette.gray99, width: 1)),
+                              child: Text(
+                                '40',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                            )
+                          ],
+                        ),
+                        trailing: Icon(
+                          Icons.arrow_forward_ios,
+                          size: 16,
+                        ),
+                      );
+                    },
+                  ),
+                );/*  */
+              })
+            ],
+          ),
         ),
       ),
     );
