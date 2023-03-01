2b903e4b6d1f5184228cd71b1e3145bd534c2374
Versy <sohnsmith@sonjaehyeong-ui-MacBookPro.local>
2023-03-01 10:15:42 +0900

    null check

--- lib/app/controller/memberTicket_controller.dart
+++ lib/app/controller/memberTicket_controller.dart
@@ -38,16 +38,22 @@ class MemberTicketController {
 
     // Tap한 티켓만 선택상태로 변경
     var item = ticketList.firstWhere(
-        (element) => element['id'] == currentStateTicketList[index]['id']);
+        (element) => element['id'] == currentStateTicketList[index]['id'],
+        orElse: () => null);
     // print('[isSelect] index를 찾아라 $index');
     // print('[isSelect] ### 여기는 함수 안이다 to ticketList $index');
-    item['isSelected'] = true;
+    if (item != null) {
+      item['isSelected'] = true;
+    }
 
     var globalItem = globalList.firstWhere(
-        (element) => element['id'] == currentStateTicketList[index]['id']);
+        (element) => element['id'] == currentStateTicketList[index]['id'],
+        orElse: () => null);
     // print('[isSelect] index를 찾아라 $index');
     // print('[isSelect] ### 여기는 함수 안이다 to globalList $index');
 
-    globalItem['isSelected'] = true;
+    if (item != null) {
+      globalItem['isSelected'] = true;
+    }
   }
 }
